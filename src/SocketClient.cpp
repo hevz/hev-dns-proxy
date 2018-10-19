4f79bd450ff94bcdb97f3410ecb6c75e151d3c8b
Heiher <r@hev.cc>
2018-10-19 13:42:26 +0800

    SocketClient: Sync to new coding style.

--- src/SocketClient.cpp
+++ src/SocketClient.cpp
@@ -30,20 +30,24 @@
 
 #include "SocketClient.h"
 
-SocketClient::SocketClient(int socket, bool owned) {
-    init(socket, owned, false);
+SocketClient::SocketClient (int socket, bool owned)
+{
+    init (socket, owned, false);
 }
 
-SocketClient::SocketClient(int socket, bool owned, bool useCmdNum) {
-    init(socket, owned, useCmdNum);
+SocketClient::SocketClient (int socket, bool owned, bool useCmdNum)
+{
+    init (socket, owned, useCmdNum);
 }
 
-void SocketClient::init(int socket, bool owned, bool useCmdNum) {
+void
+SocketClient::init (int socket, bool owned, bool useCmdNum)
+{
     mSocket = socket;
     mSocketOwned = owned;
     mUseCmdNum = useCmdNum;
-    pthread_mutex_init(&mWriteMutex, NULL);
-    pthread_mutex_init(&mRefCountMutex, NULL);
+    pthread_mutex_init (&mWriteMutex, NULL);
+    pthread_mutex_init (&mRefCountMutex, NULL);
     mPid = -1;
     mUid = -1;
     mGid = -1;
@@ -51,10 +55,10 @@ void SocketClient::init(int socket, bool owned, bool useCmdNum) {
     mCmdNum = 0;
 
     struct ucred creds;
-    socklen_t szCreds = sizeof(creds);
-    memset(&creds, 0, szCreds);
+    socklen_t szCreds = sizeof (creds);
+    memset (&creds, 0, szCreds);
 
-    int err = getsockopt(socket, SOL_SOCKET, SO_PEERCRED, &creds, &szCreds);
+    int err = getsockopt (socket, SOL_SOCKET, SO_PEERCRED, &creds, &szCreds);
     if (err == 0) {
         mPid = creds.pid;
         mUid = creds.uid;
@@ -62,81 +66,92 @@ void SocketClient::init(int socket, bool owned, bool useCmdNum) {
     }
 }
 
-SocketClient::~SocketClient() {
+SocketClient::~SocketClient ()
+{
     if (mSocketOwned) {
-        close(mSocket);
+        close (mSocket);
     }
 }
 
-int SocketClient::sendMsg(int code, const char *msg, bool addErrno) {
-    return sendMsg(code, msg, addErrno, mUseCmdNum);
+int
+SocketClient::sendMsg (int code, const char *msg, bool addErrno)
+{
+    return sendMsg (code, msg, addErrno, mUseCmdNum);
 }
 
-int SocketClient::sendMsg(int code, const char *msg, bool addErrno, bool useCmdNum) {
+int
+SocketClient::sendMsg (int code, const char *msg, bool addErrno, bool useCmdNum)
+{
     char *buf;
     int ret = 0;
 
     if (addErrno) {
         if (useCmdNum) {
-            ret = asprintf(&buf, "%d %d %s (%s)", code, getCmdNum(), msg, strerror(errno));
+            ret = asprintf (&buf, "%d %d %s (%s)", code, getCmdNum (), msg,
+                            strerror (errno));
         } else {
-            ret = asprintf(&buf, "%d %s (%s)", code, msg, strerror(errno));
+            ret = asprintf (&buf, "%d %s (%s)", code, msg, strerror (errno));
         }
     } else {
         if (useCmdNum) {
-            ret = asprintf(&buf, "%d %d %s", code, getCmdNum(), msg);
+            ret = asprintf (&buf, "%d %d %s", code, getCmdNum (), msg);
         } else {
-            ret = asprintf(&buf, "%d %s", code, msg);
+            ret = asprintf (&buf, "%d %s", code, msg);
         }
     }
     // Send the zero-terminated message
     if (ret != -1) {
-        ret = sendMsg(buf);
-        free(buf);
+        ret = sendMsg (buf);
+        free (buf);
     }
     return ret;
 }
 
 // send 3-digit code, null, binary-length, binary data
-int SocketClient::sendBinaryMsg(int code, const void *data, int len) {
-
+int
+SocketClient::sendBinaryMsg (int code, const void *data, int len)
+{
     // 4 bytes for the code & null + 4 bytes for the len
     char buf[8];
     // Write the code
-    snprintf(buf, 4, "%.3d", code);
+    snprintf (buf, 4, "%.3d", code);
     // Write the len
-    uint32_t tmp = htonl(len);
-    memcpy(buf + 4, &tmp, sizeof(uint32_t));
+    uint32_t tmp = htonl (len);
+    memcpy (buf + 4, &tmp, sizeof (uint32_t));
 
     struct iovec vec[2];
-    vec[0].iov_base = (void *) buf;
-    vec[0].iov_len = sizeof(buf);
-    vec[1].iov_base = (void *) data;
+    vec[0].iov_base = (void *)buf;
+    vec[0].iov_len = sizeof (buf);
+    vec[1].iov_base = (void *)data;
     vec[1].iov_len = len;
 
-    pthread_mutex_lock(&mWriteMutex);
-    int result = sendDataLockedv(vec, (len > 0) ? 2 : 1);
-    pthread_mutex_unlock(&mWriteMutex);
+    pthread_mutex_lock (&mWriteMutex);
+    int result = sendDataLockedv (vec, (len > 0) ? 2 : 1);
+    pthread_mutex_unlock (&mWriteMutex);
 
     return result;
 }
 
 // Sends the code (c-string null-terminated).
-int SocketClient::sendCode(int code) {
+int
+SocketClient::sendCode (int code)
+{
     char buf[4];
-    snprintf(buf, sizeof(buf), "%.3d", code);
-    return sendData(buf, sizeof(buf));
+    snprintf (buf, sizeof (buf), "%.3d", code);
+    return sendData (buf, sizeof (buf));
 }
 
-char *SocketClient::quoteArg(const char *arg) {
-    int len = strlen(arg);
-    char *result = (char *)malloc(len * 2 + 3);
+char *
+SocketClient::quoteArg (const char *arg)
+{
+    int len = strlen (arg);
+    char *result = (char *)malloc (len * 2 + 3);
     char *current = result;
     const char *end = arg + len;
     char *oldresult;
 
-    if(result == NULL) {
-        SLOGW("malloc error (%s)", strerror(errno));
+    if (result == NULL) {
+        SLOGW ("malloc error (%s)", strerror (errno));
         return NULL;
     }
 
@@ -153,42 +168,48 @@ char *SocketClient::quoteArg(const char *arg) {
     *(current++) = '"';
     *(current++) = '\0';
     oldresult = result; // save pointer in case realloc fails
-    result = (char *)realloc(result, current-result);
+    result = (char *)realloc (result, current - result);
     return result ? result : oldresult;
 }
 
-
-int SocketClient::sendMsg(const char *msg) {
+int
+SocketClient::sendMsg (const char *msg)
+{
     // Send the message including null character
-    if (sendData(msg, strlen(msg) + 1) != 0) {
-        SLOGW("Unable to send msg '%s'", msg);
+    if (sendData (msg, strlen (msg) + 1) != 0) {
+        SLOGW ("Unable to send msg '%s'", msg);
         return -1;
     }
     return 0;
 }
 
-int SocketClient::sendData(const void *data, int len) {
+int
+SocketClient::sendData (const void *data, int len)
+{
     struct iovec vec[1];
-    vec[0].iov_base = (void *) data;
+    vec[0].iov_base = (void *)data;
     vec[0].iov_len = len;
 
-    pthread_mutex_lock(&mWriteMutex);
-    int rc = sendDataLockedv(vec, 1);
-    pthread_mutex_unlock(&mWriteMutex);
+    pthread_mutex_lock (&mWriteMutex);
+    int rc = sendDataLockedv (vec, 1);
+    pthread_mutex_unlock (&mWriteMutex);
 
     return rc;
 }
 
-int SocketClient::sendDatav(struct iovec *iov, int iovcnt) {
-    pthread_mutex_lock(&mWriteMutex);
-    int rc = sendDataLockedv(iov, iovcnt);
-    pthread_mutex_unlock(&mWriteMutex);
+int
+SocketClient::sendDatav (struct iovec *iov, int iovcnt)
+{
+    pthread_mutex_lock (&mWriteMutex);
+    int rc = sendDataLockedv (iov, iovcnt);
+    pthread_mutex_unlock (&mWriteMutex);
 
     return rc;
 }
 
-int SocketClient::sendDataLockedv(struct iovec *iov, int iovcnt) {
-
+int
+SocketClient::sendDataLockedv (struct iovec *iov, int iovcnt)
+{
     if (mSocket < 0) {
         errno = EHOSTUNREACH;
         return -1;
@@ -203,13 +224,13 @@ int SocketClient::sendDataLockedv(struct iovec *iov, int iovcnt) {
     int current = 0;
 
     struct sigaction new_action, old_action;
-    memset(&new_action, 0, sizeof(new_action));
+    memset (&new_action, 0, sizeof (new_action));
     new_action.sa_handler = SIG_IGN;
-    sigaction(SIGPIPE, &new_action, &old_action);
+    sigaction (SIGPIPE, &new_action, &old_action);
 
     for (;;) {
-        ssize_t rc = TEMP_FAILURE_RETRY(
-            writev(mSocket, iov + current, iovcnt - current));
+        ssize_t rc = TEMP_FAILURE_RETRY (
+            writev (mSocket, iov + current, iovcnt - current));
 
         if (rc > 0) {
             size_t written = rc;
@@ -227,16 +248,16 @@ int SocketClient::sendDataLockedv(struct iovec *iov, int iovcnt) {
 
         if (rc == 0) {
             e = EIO;
-            SLOGW("0 length write :(");
+            SLOGW ("0 length write :(");
         } else {
             e = errno;
-            SLOGW("write error (%s)", strerror(e));
+            SLOGW ("write error (%s)", strerror (e));
         }
         ret = -1;
         break;
     }
 
-    sigaction(SIGPIPE, &old_action, &new_action);
+    sigaction (SIGPIPE, &old_action, &new_action);
 
     if (e != 0) {
         errno = e;
@@ -244,22 +265,26 @@ int SocketClient::sendDataLockedv(struct iovec *iov, int iovcnt) {
     return ret;
 }
 
-void SocketClient::incRef() {
-    pthread_mutex_lock(&mRefCountMutex);
+void
+SocketClient::incRef ()
+{
+    pthread_mutex_lock (&mRefCountMutex);
     mRefCount++;
-    pthread_mutex_unlock(&mRefCountMutex);
+    pthread_mutex_unlock (&mRefCountMutex);
 }
 
-bool SocketClient::decRef() {
+bool
+SocketClient::decRef ()
+{
     bool deleteSelf = false;
-    pthread_mutex_lock(&mRefCountMutex);
+    pthread_mutex_lock (&mRefCountMutex);
     mRefCount--;
     if (mRefCount == 0) {
         deleteSelf = true;
     } else if (mRefCount < 0) {
-        SLOGE("SocketClient refcount went negative!");
+        SLOGE ("SocketClient refcount went negative!");
     }
-    pthread_mutex_unlock(&mRefCountMutex);
+    pthread_mutex_unlock (&mRefCountMutex);
     if (deleteSelf) {
         delete this;
     }
--- src/SocketClient.h
+++ src/SocketClient.h
@@ -24,9 +24,10 @@
 #include <sys/uio.h>
 #include <stdatomic.h>
 
-class SocketClient {
-    int             mSocket;
-    bool            mSocketOwned;
+class SocketClient
+{
+    int mSocket;
+    bool mSocketOwned;
     pthread_mutex_t mWriteMutex;
 
     // Peer process ID
@@ -47,58 +48,76 @@ class SocketClient {
     bool mUseCmdNum;
 
 public:
-    SocketClient(int sock, bool owned);
-    SocketClient(int sock, bool owned, bool useCmdNum);
-    virtual ~SocketClient();
+    SocketClient (int sock, bool owned);
+    SocketClient (int sock, bool owned, bool useCmdNum);
+    virtual ~SocketClient ();
 
-    int getSocket() { return mSocket; }
-    pid_t getPid() const { return mPid; }
-    uid_t getUid() const { return mUid; }
-    gid_t getGid() const { return mGid; }
-    void setCmdNum(int cmdNum) {
-        volatile atomic_int_least32_t* a = (volatile atomic_int_least32_t*)&mCmdNum;
-        atomic_store_explicit(a, cmdNum, memory_order_release);
+    int getSocket ()
+    {
+        return mSocket;
     }
-    int getCmdNum() { return mCmdNum; }
+    pid_t getPid () const
+    {
+        return mPid;
+    }
+    uid_t getUid () const
+    {
+        return mUid;
+    }
+    gid_t getGid () const
+    {
+        return mGid;
+    }
+    void setCmdNum (int cmdNum)
+    {
+        volatile atomic_int_least32_t *a =
+            (volatile atomic_int_least32_t *)&mCmdNum;
+        atomic_store_explicit (a, cmdNum, memory_order_release);
+    }
+    int getCmdNum ()
+    {
+        return mCmdNum;
+    }
 
     // Send null-terminated C strings:
-    int sendMsg(int code, const char *msg, bool addErrno);
-    int sendMsg(int code, const char *msg, bool addErrno, bool useCmdNum);
-    int sendMsg(const char *msg);
+    int sendMsg (int code, const char *msg, bool addErrno);
+    int sendMsg (int code, const char *msg, bool addErrno, bool useCmdNum);
+    int sendMsg (const char *msg);
 
     // Provides a mechanism to send a response code to the client.
     // Sends the code and a null character.
-    int sendCode(int code);
+    int sendCode (int code);
 
     // Provides a mechanism to send binary data to client.
     // Sends the code and a null character, followed by 4 bytes of
     // big-endian length, and the data.
-    int sendBinaryMsg(int code, const void *data, int len);
+    int sendBinaryMsg (int code, const void *data, int len);
 
     // Sending binary data:
-    int sendData(const void *data, int len);
+    int sendData (const void *data, int len);
     // iovec contents not preserved through call
-    int sendDatav(struct iovec *iov, int iovcnt);
+    int sendDatav (struct iovec *iov, int iovcnt);
 
     // Optional reference counting.  Reference count starts at 1.  If
     // it's decremented to 0, it deletes itself.
     // SocketListener creates a SocketClient (at refcount 1) and calls
     // decRef() when it's done with the client.
-    void incRef();
-    bool decRef(); // returns true at 0 (but note: SocketClient already deleted)
+    void incRef ();
+    bool
+    decRef (); // returns true at 0 (but note: SocketClient already deleted)
 
     // return a new string in quotes with '\\' and '\"' escaped for "my arg"
     // transmissions
-    static char *quoteArg(const char *arg);
+    static char *quoteArg (const char *arg);
 
 private:
-    void init(int socket, bool owned, bool useCmdNum);
+    void init (int socket, bool owned, bool useCmdNum);
 
     // Sending binary data. The caller should make sure this is protected
     // from multiple threads entering simultaneously.
     // returns 0 if successful, -1 if there is a 0 byte write or if any
     // other error occurred (use errno to get the error)
-    int sendDataLockedv(struct iovec *iov, int iovcnt);
+    int sendDataLockedv (struct iovec *iov, int iovcnt);
 };
 
 typedef android::sysutils::List<SocketClient *> SocketClientCollection;
