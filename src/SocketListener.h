a3292dd534effecad4437d64a19fda0f8fd59e52
Heiher <r@hev.cc>
2018-10-19 13:41:25 +0800

    SocketListener: Sync to new coding style.

--- src/SocketListener.h
+++ src/SocketListener.h
@@ -21,39 +21,44 @@
 #include "SocketClient.h"
 #include "SocketClientCommand.h"
 
-class SocketListener {
-    bool                    mListen;
-    const char              *mSocketName;
-    int                     mSock;
-    SocketClientCollection  *mClients;
-    pthread_mutex_t         mClientsLock;
-    int                     mCtrlPipe[2];
-    pthread_t               mThread;
-    bool                    mUseCmdNum;
+class SocketListener
+{
+    bool mListen;
+    const char *mSocketName;
+    int mSock;
+    SocketClientCollection *mClients;
+    pthread_mutex_t mClientsLock;
+    int mCtrlPipe[2];
+    pthread_t mThread;
+    bool mUseCmdNum;
 
 public:
-    SocketListener(const char *socketName, bool listen);
-    SocketListener(const char *socketName, bool listen, bool useCmdNum);
-    SocketListener(int socketFd, bool listen);
+    SocketListener (const char *socketName, bool listen);
+    SocketListener (const char *socketName, bool listen, bool useCmdNum);
+    SocketListener (int socketFd, bool listen);
 
-    virtual ~SocketListener();
-    int startListener();
-    int startListener(int backlog);
-    int stopListener();
+    virtual ~SocketListener ();
+    int startListener ();
+    int startListener (int backlog);
+    int stopListener ();
 
-    void sendBroadcast(int code, const char *msg, bool addErrno);
+    void sendBroadcast (int code, const char *msg, bool addErrno);
 
-    void runOnEachSocket(SocketClientCommand *command);
+    void runOnEachSocket (SocketClientCommand *command);
 
-    bool release(SocketClient *c) { return release(c, true); }
+    bool release (SocketClient *c)
+    {
+        return release (c, true);
+    }
 
 protected:
-    virtual bool onDataAvailable(SocketClient *c) = 0;
+    virtual bool onDataAvailable (SocketClient *c) = 0;
 
 private:
-    bool release(SocketClient *c, bool wakeup);
-    static void *threadStart(void *obj);
-    void runListener();
-    void init(const char *socketName, int socketFd, bool listen, bool useCmdNum);
+    bool release (SocketClient *c, bool wakeup);
+    static void *threadStart (void *obj);
+    void runListener ();
+    void init (const char *socketName, int socketFd, bool listen,
+               bool useCmdNum);
 };
 #endif
